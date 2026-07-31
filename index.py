1928594759bfbc41097f831189f26f7bf135ba43
Umar Bin Riaz <umar.bin.riaz99@gmail.com>
2026-07-31 16:11:51 +0500

    Add grade calculator in Python

---
+++ index.py
@@ -0,0 +1,18 @@
+# Grade Calculator in Python
+
+marks = float(input("Enter your marks (0-100): "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your Grade:", grade)
